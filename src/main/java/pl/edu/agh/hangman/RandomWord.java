3725659eb9f5028bd989bfbe02d44357149b65a9
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-01-23 13:44:53 +0100

    Added getRandomWord method

--- src/main/java/pl/edu/agh/hangman/RandomWord.java
+++ src/main/java/pl/edu/agh/hangman/RandomWord.java
@@ -1,5 +1,13 @@
 package pl.edu.agh.hangman;
 
+import java.util.List;
+import java.util.Random;
+
 public class RandomWord {
+
+  public static String getRandomWord(List<String> listOfWords) {
+    Random rand = new Random();
+    return listOfWords.get(rand.nextInt(listOfWords.size()));
+  }
 
 }
