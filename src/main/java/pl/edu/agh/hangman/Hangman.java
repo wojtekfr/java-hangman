6a0e90a44d52747d197977e6c70a6b301537ddc9
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-01-23 14:38:53 +0100

    Added printStringArray method

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.hangman;
 
+import java.util.List;
+
 public class Hangman {
 	
 	
@@ -58,24 +60,44 @@ public class Hangman {
     };
 
     public static void main(String[] args) {
+        printStringArray(new String[]{"_", "_", "_"});
     }
-    	
-    		
-    	public static String drawEmptyWord(String word){
-    	    int num = word.length();
-    	    String emptyWord = "";
-    	    for (int i=0; i<num; i++){
-    	        emptyWord += "_";
-    	    }
-    	    return emptyWord;
-    	}
-    	
-    	public String drawLetter(char letter, String word){
-    	     = word.length();
-    	    String emptyWord = "";
-    	    for (int i=0; i<num; i++){
-    	        emptyWord += "_";
-    	    }
-    	    return emptyWord;
+
+    public static String w(String word){
+        int num = word.length();
+        String emptyWord = "";
+        for (int i=0; i<num; i++){
+            emptyWord += "_";
+        }
+        return emptyWord;
     }
+
+
+    public static String[] drawEmptyWord(String word) {
+        int num = word.length();
+        String[] emptyWord = new String[word.length()];
+        for (int i = 0; i < num; i++) {
+            emptyWord[i] = "_";
+        }
+        return emptyWord;
+    }
+
+    public String[] drawLetter(String letter, String[] wordArray, String word) {
+        List<Integer> indexes = LetterOperations.verifyLetter(letter, word);
+        if (indexes.isEmpty()) {
+            return wordArray;
+        } else {
+            for (Integer index: indexes) {
+                wordArray[index] = letter;
+            }
+        }
+        return wordArray;
+    }
+
+    public static void printStringArray(String[] array) {
+        for (String letter: array) {
+            System.out.print(letter + " ");
+        }
+    }
+
 }
