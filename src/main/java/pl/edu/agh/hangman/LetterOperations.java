d5e5f54b9b6a5b04f954f23ebb82c167d938a451
Wojciech Frabinski <wojtef@gmail.com>
2021-01-23 13:30:20 +0100

    classes created

---
+++ src/main/java/pl/edu/agh/hangman/LetterOperations.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.hangman;
+
+import java.util.Scanner;
+
+public class LetterOperations {
+	Scanner scanner = new Scanner(System.in);
+	
+	public String getLetter() {
+		System.out.print();	
+		return null;
+	}
+	
+	
+}
