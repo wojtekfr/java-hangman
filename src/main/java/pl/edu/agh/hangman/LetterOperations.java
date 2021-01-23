0f4d7aace1fb15ec0b48f9346335a0dba8c7731c
Wojciech Frabinski <wojtef@gmail.com>
2021-01-23 13:56:09 +0100

    Letter opereratons updated

--- src/main/java/pl/edu/agh/hangman/LetterOperations.java
+++ src/main/java/pl/edu/agh/hangman/LetterOperations.java
@@ -4,11 +4,27 @@ import java.util.Scanner;
 
 public class LetterOperations {
 	Scanner scanner = new Scanner(System.in);
-	
+	String letter;
+
 	public String getLetter() {
-		System.out.print();	
-		return null;
+		do {
+			System.out.print("Podaj literę: ");
+			letter = scanner.next();
+			if (letter.length() != 1) {
+				System.out.println("Podaj tylko jedną literę");
+			}
+		} while (letter.length() == 1);
+		return letter;
+	}
+	public int verifyLetter(String letter, String word) {
+		
+		for (int index = 1; index <= word.length(); index++) {
+			if (word.charAt(index) == letter.charAt(0)) {
+					return index; 
+			}
+		}
+		return 0;
 	}
 	
-	
+
 }
