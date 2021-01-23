72a0af0e9c7752f890de40dab0625e3224c05f68
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-01-23 13:40:52 +0100

    Added getListOfWords method

--- src/main/java/pl/edu/agh/hangman/FileWordReader.java
+++ src/main/java/pl/edu/agh/hangman/FileWordReader.java
@@ -1,5 +1,27 @@
 package pl.edu.agh.hangman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileWordReader {
+
+  private static List<String> getListOfWords() {
+    File fileToRead = new File("src/main/resources/slowa.txt");
+    List<String> listOfWords = new ArrayList<>();
+    try {
+      Scanner myReader = new Scanner(fileToRead);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        listOfWords.add(data.toUpperCase());
+      }
+    } catch (
+        FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return listOfWords;
+  }
 
 }
